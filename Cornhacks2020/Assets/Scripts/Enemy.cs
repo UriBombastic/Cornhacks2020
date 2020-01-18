50a8fa9cf32a10dfcabf4ad0cc845ef0f5ae13cd
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 14:34:50 -0600

    Trying to add enemy spawner

--- Cornhacks2020/Assets/Scripts/Enemy.cs
+++ Cornhacks2020/Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Vector2 moveDir;
+    public float maxSpeed =5f;
     private Rigidbody2D rb;
     private void Awake()
     {
@@ -15,6 +16,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         rb.AddForce(moveDir);
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
     }
 
      void OnCollisionEnter2D(Collision2D collision)
---
+++ Cornhacks2020/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Vector3 MoveDirection;
+    public float maxSpeed = 5f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(MoveDirection);
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        MoveDirection *= -1;
+    }
+}
