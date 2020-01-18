74f6af710fd71894108c5e9113a5403c8ddea42c
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 13:12:28 -0600

    Added random platform generation

---
+++ Cornhacks2020/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public Transform[] spawnSpaces;
+    public GameObject[] platforms;
+    public int minPlatforms;
+    public int maxPlatforms;
+    void Start()
+    {
+        GeneratePlatforms();
+    }
+
+  void GeneratePlatforms()
+    {
+        int numPlatforms = Random.Range(minPlatforms, maxPlatforms);
+        for(int i = 0; i < numPlatforms; i++)
+        {
+            int platformIndex = Random.Range(0, platforms.Length);
+            int spawnIndex = Random.Range(0, spawnSpaces.Length);
+            Instantiate(platforms[platformIndex], spawnSpaces[spawnIndex]);
+
+        }
+    }
+
+
+}
