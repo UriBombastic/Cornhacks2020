3627e337cfe9c7d1004b2820a5b55bf9934a409b
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 15:40:32 -0600

    Added enemy spawner

--- Cornhacks2020/Assets/Scripts/EnemySpawner.cs
+++ Cornhacks2020/Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,9 @@ public class EnemySpawner : MonoBehaviour
     public Vector3 MoveDirection;
     public float maxSpeed = 5f;
     private Rigidbody2D rb;
+    public float SpawnTime = 1.5f;
+    public bool canSpawn = true;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,19 @@ public class EnemySpawner : MonoBehaviour
     {
         rb.AddForce(MoveDirection);
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
+        if (canSpawn) StartCoroutine(SpawnEnemy());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         MoveDirection *= -1;
+    }
+
+    public IEnumerator SpawnEnemy()
+    {
+        canSpawn = false;
+        Instantiate(enemy, transform);
+        yield return new WaitForSeconds(SpawnTime);
+        canSpawn = true;
     }
 }
