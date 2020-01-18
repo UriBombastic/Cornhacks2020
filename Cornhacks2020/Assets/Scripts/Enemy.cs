eaf4829bd1c3ab36c5cf9f6b41dfbb1eff4f095e
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 13:54:20 -0600

    Added an enemy

---
+++ Cornhacks2020/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Vector2 moveDir;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(moveDir);
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
