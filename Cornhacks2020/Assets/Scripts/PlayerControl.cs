87206a469e1b276f1d0641a9ed29e43d735e0687
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 13:59:28 -0600

    Merge with Alex

--- Cornhacks2020/Assets/Scripts/PlayerControl.cs
+++ Cornhacks2020/Assets/Scripts/PlayerControl.cs
@@ -13,6 +13,7 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D rb;
     public bool isJumping;
     public float JumpDelay = 0.5f;
+    public float distFromGround;
 
     private void Awake()
     {
@@ -22,26 +23,47 @@ public class PlayerControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Health = MaxHealth;    
+        Health = MaxHealth;   
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        distFromGround = transform.localPosition.y-((float)-4.17);
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Move(h, v);
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed));
+       
     }
 
     void Move(float h, float v)
     {
-        Vector2 moveDirection = new Vector2(h, (v>0?0 : v)) * MoveForce;
-        rb.AddForce(moveDirection);
-        if(v > 0 && !isJumping)
+        if (h != 0 || v != 0)
         {
-            rb.AddForce(new Vector2(0, jumpForce));
-            StartCoroutine(handleJumping());
+            Vector2 moveDirection = new Vector2(h, (v > 0 ? 0 : v)) * MoveForce;
+            rb.AddForce(moveDirection);
+            //flips sprite according to velocity
+            if (h < 0 && transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            }
+            else if (h > 0 && transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            }
+            
+            if (v > 0 && !isJumping)
+            {
+                rb.AddForce(new Vector2(0, jumpForce));
+                StartCoroutine(handleJumping());
+            }
+        }
+        if (h == 0 && transform.position.y <= -4)
+        {
+            rb.velocity = new Vector2((float)0.0, rb.velocity.y);
         }
     }
 
@@ -50,6 +72,11 @@ public class PlayerControl : MonoBehaviour
         isJumping = true;
         yield return new WaitForSeconds(JumpDelay);
         isJumping = false;
+    }
+
+    private bool OnGround()
+    {
+        return Physics.Raycast(transform.position,-Vector2.up,distFromGround+(float).1);
     }
 
 }
