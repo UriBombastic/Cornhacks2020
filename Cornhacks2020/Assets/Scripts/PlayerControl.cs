8e527985aa06dbadc568523ac53357dc297a6814
Uri Herszbaum-Harding <uherszbaum-harding2@unl.edu>
2020-01-18 12:16:41 -0600

    Setting up player

---
+++ Cornhacks2020/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float MoveForce;
+    public float jumpForce = 10f;
+    public float MaxHealth;
+    public float Health;
+    public int MaxJumps;
+    private Rigidbody2D rb;
+    public bool isJumping;
+    public float JumpDelay = 0.5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = MaxHealth;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h, v);
+
+    }
+
+    void Move(float h, float v)
+    {
+        Vector2 moveDirection = new Vector2(h, (v>0?0 : v)) * MoveForce;
+        rb.AddForce(moveDirection);
+        if(v > 0 && !isJumping)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            StartCoroutine(handleJumping());
+        }
+    }
+
+    private IEnumerator handleJumping()
+    {
+        isJumping = true;
+        yield return new WaitForSeconds(JumpDelay);
+        isJumping = false;
+    }
+
+}
